cb7e40c47e405015b5f454dd6de15ba24f3df27e
Andrey Golubkov <Golubkov.Andrew@gmail.com>
2025-06-05 15:04:34 +0300

    Add license path argument

--- Demos/UI/TextExtractor/Program.cs
+++ Demos/UI/TextExtractor/Program.cs
@@ -12,9 +12,10 @@ namespace GroupDocs.Parser.TextExtractor
     {
         static void Main(string[] args)
         {
-            if (args.Length != 4)
+            if (args.Length != 5)
             {
                 Console.WriteLine("Command line arguments: ");
+                Console.WriteLine("\tLicense file path");
                 Console.WriteLine("\tDocument file path");
                 Console.WriteLine("\tTemplate file path");
                 Console.WriteLine("\tOutput file path");
@@ -22,17 +23,27 @@ namespace GroupDocs.Parser.TextExtractor
                 Environment.Exit(5);
             }
 
-            var documentPath = args[0];
-            var templatePath = args[1];
-            var outputPath = args[2];
-            bool useOcr = bool.Parse(args[3]);
+            var licensePath = args[0];
+            var documentPath = args[1];
+            var templatePath = args[2];
+            var outputPath = args[3];
+            bool useOcr = bool.Parse(args[4]);
+            Console.WriteLine("License file path: " + licensePath);
             Console.WriteLine("Document file path: " + documentPath);
             Console.WriteLine("Template file path: " + templatePath);
             Console.WriteLine("Output file path: " + outputPath);
             Console.WriteLine("Use OCR: " + useOcr);
+            Console.WriteLine();
 
             var template = Template.Load(templatePath);
             var stringBuilder = new StringBuilder();
+
+            Console.WriteLine("Setting the license");
+            new License().SetLicense(licensePath);
+            Console.WriteLine("The license is set");
+            Console.WriteLine();
+
+            Console.WriteLine("Parsing the template");
             using (Parser parser = new Parser(documentPath))
             {
                 var options = new ParseByTemplateOptions(useOcr);
@@ -64,6 +75,7 @@ namespace GroupDocs.Parser.TextExtractor
             }
 
             File.WriteAllText(outputPath, stringBuilder.ToString());
+            Console.WriteLine("All done");
         }
     }
 }
